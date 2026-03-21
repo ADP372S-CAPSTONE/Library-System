9be0d16b31210b2984d7c2c6d05ee3018eef5fe8
Sinazo Ntsimbi <sinazo.ntsimbi@gmail.com>
2026-03-22 00:20:46 +0200

    Updated Loan&LoanFactory.Added LoanFactoryTest

--- src/main/java/factory/LoanFactory.java
+++ src/main/java/factory/LoanFactory.java
@@ -1,6 +1,6 @@
 
 package factory;
-/**
+/*
  *Author: Sinazo Ntsimbi
  * 222765208
  */
@@ -13,7 +13,7 @@ public class LoanFactory {
     public static Loan createLoan(String loanId, LocalDate issueDate, LocalDate dueDate, LocalDate returnDate) {
         return new Loan.Builder()
                 .setLoanId(loanId)
-                .setIssue(issueDate)
+                .setIssueDate(issueDate)
                 .setDueDate(dueDate)
                 .setReturnDate(returnDate)
                 .build();
